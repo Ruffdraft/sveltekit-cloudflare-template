3368fe593aad5caba27b3824784bdb46ff04baa7
Ruffdraft <polysync1@googlemail.com>
2025-05-30 15:42:06 +0100

    Create +page.svelte

---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,7 @@
+<form method="POST" enctype="multipart/form-data">
+	<label>
+		Select PCAP file:
+		<input type="file" name="file" accept=".pcap" required />
+	</label>
+	<button type="submit" class="btn">Upload</button>
+</form>
